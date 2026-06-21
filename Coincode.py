8b6d5fb91bf8577258a075564a7a3f3e1b63cd62
Hitesh Kumar Nandwani <48645174+hitesh2511@users.noreply.github.com>
2026-06-21 22:06:41 +0530

    Update Coincode.py

--- Coincode.py
+++ Coincode.py
@@ -30,8 +30,8 @@ DELTA_API_URL = "https://api.india.delta.exchange"
 
 # --- DISCORD CONFIG -----------------------------------------------------------
 # Replace these with your actual values:
-DISCORD_BOT_TOKEN  = st.secret["BOT"]   # From Discord Developer Portal
-DISCORD_CHANNEL_ID = st.secret["BOTID"]          # Right-click channel → Copy ID
+DISCORD_BOT_TOKEN  = st.secrets["BOT"]   # From Discord Developer Portal
+DISCORD_CHANNEL_ID = st.secrets["BOTID"]          # Right-click channel → Copy ID
 
 CANDLE_PCT_THRESHOLD = 0.6    # minimum absolute % move on 5-min candle
 ALERT_COOLDOWN_SEC   = 300    # seconds before re-alerting same symbol
